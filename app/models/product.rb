314aacc37b50f9f4d79a65574cea039ddf1df565
Antonino <antoninoscaffidi@gmail.com>
2018-04-18 15:41:30 +0200

    Added validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  validates :title, :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
   has_one_attached :cover
   #has_many_attached :images
 end
