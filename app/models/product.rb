20898cd152a425791dd281c34652c9d268e9b3c7
Antonino <antoninoscaffidi@gmail.com>
2018-04-30 10:57:07 +0200

    Added test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
-  validates :title, :description, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-
   has_one_attached :cover
   #has_many_attached :images
+  
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+  #validates :cover, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
 end
