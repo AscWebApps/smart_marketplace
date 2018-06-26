a343e4b7c90b14b46e2617995f452abdfcb6540c
Antonino <antoninoscaffidi@gmail.com>
2018-06-26 18:09:28 +0200

    Added a callback before_destroy in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   
   has_many :line_items, dependent: :destroy
-  
+  before_destroy :ensure_not_referenced_by_any_line_item 
   
   
   validates :title, :description, presence: true
@@ -11,6 +11,14 @@ class Product < ApplicationRecord
 
   has_one_attached :cover
   #has_many_attached :images
+ 
+  private
   
+  def ensure_not_referenced_by_any_linr_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end 
 
 end
